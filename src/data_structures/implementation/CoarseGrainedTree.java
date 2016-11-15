828f8f6a1f9aa6c19e9dd58ebfc63bcd6f7043b7
Bas van der Borden <bascasper@gmail.com>
2016-11-15 14:55:24 +0100

    added node class to CoarseGrainedTree.java

--- src/data_structures/implementation/CoarseGrainedTree.java
+++ src/data_structures/implementation/CoarseGrainedTree.java
@@ -1,10 +1,29 @@
 package data_structures.implementation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.utils.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import data_structures.Sorted;
 
 public class CoarseGrainedTree<T extends Comparable<T>> implements Sorted<T> {
+    
+    public class Node {
+        T item;
+        int key;
+        Node left;
+        Node right;
+        
+        Public Node(int key) {
+            this.key = key;
+        }
+    }
+    
+    private Node root;
+    private Lock lock = new ReentrantLock();
+    
 
     public void add(T t) {
         throw new UnsupportedOperationException();
