93e8f76e8625f60e88082299cb4744e5c8ab076a
Bas van der Borden <bascasper@gmail.com>
2016-11-15 16:13:34 +0100

    test

--- src/data_structures/implementation/CoarseGrainedTree.java
+++ src/data_structures/implementation/CoarseGrainedTree.java
@@ -1,10 +1,28 @@
 package data_structures.implementation;
 
 import java.util.ArrayList;
+import java.util.Array;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import data_structures.Sorted;
 
 public class CoarseGrainedTree<T extends Comparable<T>> implements Sorted<T> {
+	
+	public class Node {
+		T item;
+		int key;
+		Node left;
+		Node right;
+		
+		public Node(int key) {
+			this.key = key;
+		}
+	}
+	
+	private Node root;
+	private Lock lock = new ReentrantLock();
 
     public void add(T t) {
         throw new UnsupportedOperationException();
