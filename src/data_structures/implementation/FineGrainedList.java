c7160e49a2ddb1d04bb8ddcb4054150c93ba59ba
Bas van der Borden <bascasper@gmail.com>
2016-12-07 21:33:04 +0100

    Book implementations

--- src/data_structures/implementation/FineGrainedList.java
+++ src/data_structures/implementation/FineGrainedList.java
@@ -1,17 +1,86 @@
 package data_structures.implementation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import data_structures.Sorted;
 
 public class FineGrainedList<T extends Comparable<T>> implements Sorted<T> {
+	public class Node {
+		T item;
+		int key;
+		Node next;
+		
+		public Node(int key) {
+			this.key = key;
+		}
+	}
+	
+	private Node head;
+	private Lock lock = new Reentrantlock();
+	public FineGrainedList() {
+		head = new Node(Integer.MIN_VALUE);
+		head.next = new Node(Integer.MAX_VALUE);
+	}
 
-    public void add(T t) {
-        throw new UnsupportedOperationException();
+    public void add(T t) throws UnsupportedOperationException {
+        int key = item.hashCode();
+        pred.lock();
+        Node pred = head;
+        try {
+        	Node curr = pred.next;
+        	curr.lock();
+        	try {
+        		while (curr.key < key) {
+        			pred.unlock();
+        			pred = curr;
+        			curr = curr.next;
+        			curr.lock();
+        		}
+        		if (curr.key == key) {
+        			return false;
+        		}
+        		Node newNode = new Node(item);
+        		newNode.next = curr;
+        		pred.next = newNode;
+        		return true;
+        	} finally {
+        		curr.unlock();
+        	}
+        } finally {
+        	pred.unlock();
+        }
     }
 
-    public void remove(T t) {
-        throw new UnsupportedOperationException();
+    public void remove(T t) throws UnsupportedOperationException {
+        Node pred = null, curr = null;
+        int key = item.hashCode();
+        pred.lock();
+        try {
+        	pred = head;
+        	curr = pred.next;
+        	curr.lock();
+        	try {
+        		while (curr.key < key) {
+        			pred.unlock();
+        			pred = curr;
+        			curr = curr.next;
+        			curr.lock();
+        		}
+        		if (curr.key == key) {
+        			pred.next = curr.next;
+        			return true;
+        		}
+        		return false;
+        	} finally {
+        		curr.unlock();
+        	}
+        } finally {
+        	pred.unlock();
+        }
     }
 
     public ArrayList<T> toArrayList() {
--- src/data_structures/implementation/FineGrainedTree.java
+++ src/data_structures/implementation/FineGrainedTree.java
@@ -1,17 +1,58 @@
 package data_structures.implementation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import data_structures.Sorted;
 
 public class FineGrainedTree<T extends Comparable<T>> implements Sorted<T> {
+	public class TreeNode {
+		Counter counter;
+		TreeNode parent, right, left;
+		Bin<T> bin;
+		public boolean isleaf() {
+			return right == null;
+		}
+	}
+	
+	private Lock lock;
+	int range;
+	List<TreeNode> leaves;
+	TreeNode root;
+	
+	public FineGrainedTree(int logRange) {
+		lock = new ReentrantLock();
+		range = (1 << logRange);
+		leaves = new ArrayList<TreeNode>(range);
+		root = null;
+	}
 
-    public void add(T t) {
-        throw new UnsupportedOperationException();
+    public void add(T t) throws UnsupportedOperationException() {
+    	lock.Lock();
+        TreeNode node = leaves.get(score);
+        node.bin.put(item);
+        while(node != root) {
+        	TreeNode parent = node.parent;
+        	if (node == parent.left) {
+        		parent.counter.getAndIncrement();
+        	}
+        	node = parent;
+        }
     }
 
-    public void remove(T t) {
-        throw new UnsupportedOperationException();
+    public void remove(T t) throws UnsupportedOperationException() {
+        TreeNode node = root;
+        while (!node.isleaf()) {
+        	if (node.counter.boundedGetAndDecrement() > 0) {
+        		node = node.left;
+        	} else {
+        		node = node.right;
+        	}
+        }
+        return node.bin.get();
     }
 
     public ArrayList<T> toArrayList() {
